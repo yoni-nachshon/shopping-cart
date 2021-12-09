09e6bcc2fd755071860db52a0b637751fe9e4ca6
Yoni Nachshon <yonin@ravtech.local>
2021-12-09 07:48:32 -0800

    Basic shopping cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Shop from "./components/Shop";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>         
+        <div style={{textAlign:'center',marginTop:'1rem'}}>
+        {/* <Link to='/' style={{ color: 'inherit', textDecoration: 'none' }}>Store</Link> */}
+        </div>    
+        <Routes>
+          <Route path="/" exact element={<Shop />} />
+        </Routes>     
+    </>
   );
 }
 
