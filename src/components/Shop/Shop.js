560a5f2925906a7c57bef26ace04257022c0e7a5
Yoni Nachshon <yonin@ravtech.local>
2022-01-19 04:06:01 -0800

    bug fix

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,7 +6,6 @@ import { style } from "./style";
 import { makeStyles } from '@mui/styles';
 import { items } from "../../data";
 
-
 const useStyles = makeStyles(style);
 
 const Shop = () => {
@@ -14,11 +13,9 @@ const Shop = () => {
     const [show, setShow] = useState(false);
 
     const [products, setProducts] = useState([])
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState('')
 
-    const [cart, setCart] = useState([]);
-    const [showInCart, setShowInCart] = useState([]);
-
+    const [cart, setCart] = useState([])
     const [loading, setLoading] = useState(true)
 
     const styles = useStyles()
@@ -26,54 +23,59 @@ const Shop = () => {
     useEffect(() => {
         setTimeout(() => {
             setProducts(items)
+            console.table(items)
             setLoading(false)
         }, 2000)
     }, [])
 
     useEffect(() => {
         setCart(JSON.parse(localStorage.getItem('cart')))
-        setShowInCart(JSON.parse(localStorage.getItem('showInCart')))
     }, [])
 
     useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cart))
-        localStorage.setItem('showInCart', JSON.stringify(showInCart))
-    }, [cart, showInCart])
+    }, [cart])
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-
-
-    const addToCart = (item) => {
-        setCart([...cart, item]);
-        if (showInCart.findIndex(i => i.id === item.id) === -1) {
-            setShowInCart([...showInCart, item])
+    const addItemToCart = (item) => {
+        const updatedCart = [...cart];
+        const updatedItemIndex = updatedCart.findIndex(i => i.id === item.id)
+        if (updatedItemIndex < 0) {
+            updatedCart.push({ ...item, quantity: 1 });
+        } else {
+            const updatedItem = {
+                ...updatedCart[updatedItemIndex]
+            }
+            updatedItem.quantity++;
+            updatedCart[updatedItemIndex] = updatedItem;
         }
+        setCart(updatedCart)
     }
 
-    const removeItem = (id) => {
-        let copy = [...cart]
-        let amount = copy.filter(i => i.id === id).length
-        if (amount > 1) {
-            let index = copy.findIndex(i => i.id === id)
-            if (index !== -1) {
-                copy.splice(index, 1)
+    const removeItemFromCart = (item) => {
+        const updatedCart = [...cart];
+        if (item.quantity > 1) {
+            const updatedItemIndex = updatedCart.findIndex(i => i.id === item.id)
+            const updatedItem = { ...updatedCart[updatedItemIndex] }
+            updatedItem.quantity--;
+            if (updatedItem.quantity <= 0) {
+                updatedCart.splice(updatedItemIndex, 1);
+            } else {
+                updatedCart[updatedItemIndex] = updatedItem;
             }
         }
-        setCart(copy)
-
+        setCart(updatedCart)
     }
 
     const removeFromCart = (item) => {
-        let copyCart = cart.filter((i) => i.id !== item.id)
-        let showCartCopy = showInCart.filter((i) => i.id !== item.id)
-        setCart(copyCart)
-        setShowInCart(showCartCopy)
+        let updatedCart = cart.filter((i) => i.id !== item.id)
+        setCart(updatedCart)
+        console.log(updatedCart);
     }
-    const cartTotal = cart.reduce((total, { price }) => total + price, 0).toFixed(2);
 
-    const amountOfItems = (item) => cart.filter(i => i.id === item.id).length;
+    const cartTotal = cart.reduce((total, { price, quantity }) => total + price * quantity, 0).toFixed(2);
 
     return loading ? (
         <div className={styles.spinner}>
@@ -114,7 +116,7 @@ const Shop = () => {
                                 <Card.Text className="mt-2">{item.title}</Card.Text>
                                 <Card.Text>${item.price}</Card.Text>
                                 <div className="d-grid gap-2">
-                                    <Button variant="warning" onClick={() => addToCart(item)}>Add To Cart</Button>
+                                    <Button variant="warning" onClick={() => addItemToCart(item)}>Add To Cart</Button>
                                 </div>
                             </Card.Body>
                         </Card>
@@ -125,19 +127,19 @@ const Shop = () => {
                         <Offcanvas.Title>Shopping Cart</Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
-                        {showInCart.length ? (
-                            showInCart.map((item) => (
-                                <Card className={styles.cart} >
+                        {cart.length ? (
+                            cart.map((item) => (
+                                <Card className={styles.cart} key={item.id}>
                                     <ListGroup variant="flush">
-                                        <ListGroup.Item key={item.id}>
+                                        <ListGroup.Item >
                                             <span style={{ float: 'right', top: '1px' }} className={styles.btn} onClick={() => removeFromCart(item)}>{xIcon}</span>
                                             <Card.Img variant="top" src={item.image} alt="" style={{ height: '90px', width: '90px' }} />
-                                            <span style={{ marginLeft: '1rem' }}>${(item.price * amountOfItems(item)).toFixed(2)}</span>
+                                            <span style={{ marginLeft: '1rem' }}>${(item.price * item.quantity).toFixed(2)}</span>
                                             <Card.Text className="mt-2">{item.title}</Card.Text>
                                             <ButtonGroup>
-                                                <Button size="sm" variant="warning" onClick={() => addToCart(item)}>{addIcon}</Button>
-                                                <Button size="sm" variant="warning" >{amountOfItems(item)}</Button>
-                                                <Button size="sm" variant="warning" onClick={() => removeItem(item.id)}>{removeIcon}</Button>
+                                                <Button size="sm" variant="warning" onClick={() => addItemToCart(item)}>{addIcon}</Button>
+                                                <Button size="sm" variant="warning" >{item.quantity}</Button>
+                                                <Button size="sm" variant="warning" onClick={() => removeItemFromCart(item)}>{removeIcon}</Button>
                                             </ButtonGroup>
                                         </ListGroup.Item>
                                     </ListGroup>
@@ -146,7 +148,7 @@ const Shop = () => {
                         ) : (
                             <div className={styles.empty} >Your cart is empty</div>
                         )}
-                        {showInCart.length > 0 && (
+                        {cart.length > 0 && (
                             <div style={{ textAlign: 'center' }} className="me-2 mt-2">
                                 subtotal ({cart.length} items):
                                 ${cartTotal}
